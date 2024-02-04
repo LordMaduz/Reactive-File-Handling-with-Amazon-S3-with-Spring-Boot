7aa5ffb590f1e9e433632a8d126a584d1b5b215e
LordMaduz <maduz.ruchira@gmail.com>
2024-02-05 00:11:03 +0800

    code changes for validations.

--- src/main/java/com/example/demo/configuration/AwsS3Config.java
+++ src/main/java/com/example/demo/configuration/AwsS3Config.java
@@ -2,10 +2,12 @@ package com.example.demo.configuration;
 
 
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.services.s3.S3AsyncClient;
 
 @RequiredArgsConstructor
@@ -21,7 +23,13 @@ public class AwsS3Config {
 
     @Bean
     AwsCredentialsProvider awsCredentialsProvider() {
-        return () -> AwsBasicCredentials.create(s3ConfigProperties.getAccessKey(), s3ConfigProperties.getSecretKey());
+        if (StringUtils.isBlank(s3ConfigProperties.getAccessKey())) {
+            return DefaultCredentialsProvider.create();
+        } else {
+            return () -> AwsBasicCredentials.create(
+                    s3ConfigProperties.getAccessKey(),
+                    s3ConfigProperties.getSecretKey());
+        }
     }
 
 }
